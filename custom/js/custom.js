6fb280c13e658762fce6060de33ad802fb051a11
Ricardo Contreras <ricardocontrerasrobles@gmail.com>
2015-11-30 08:02:17 -0800

    Adding the website

---
+++ custom/js/custom.js
@@ -0,0 +1,48 @@
+
+
+$( document ).ready(function() {    
+    $('a').click(function(){
+        $('html, body').animate({
+            scrollTop: $('[name="' + $.attr(this, 'href').substr(1) + '"]').offset().top
+        }, 500);
+        return false;
+    });
+
+});
+
+    
+    var fill = d3.scale.category20();
+
+  d3.layout.cloud().size([300, 300])
+      .words(["seizure", "convulsion", "shaking", "blackout", "confusion", "frothing", "snorting", "anger", "falling", "clenching", "spasms"].map(function(d) {
+        return {text: d, size: 10 + Math.random() * 90};
+      }))
+      .padding(5)
+      .rotate(function() { return ~~(Math.random() * 2) * 90; })
+      .font("Impact")
+      .fontSize(function(d) { return d.size; })
+      .on("end", draw)
+      .start();
+
+      
+function draw(words) {
+    d3.select("#d3cloud").append("svg")
+        .attr("width", 300)
+        .attr("height", 300)
+      .append("g")
+        .attr("transform", "translate(150,150)")
+      .selectAll("text")
+        .data(words)
+      .enter().append("text")
+        .style("font-size", function(d) { return d.size + "px"; })
+        .style("font-family", "Impact")
+        .style("fill", function(d, i) { return fill(i); })
+        .attr("text-anchor", "middle")
+        .attr("transform", function(d) {
+          return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+        })
+        .text(function(d) { return d.text; })
+        .on("click", function (d, i){
+            alert("Clicked word: " + d.text)
+        });
+  }
