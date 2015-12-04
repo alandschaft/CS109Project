2fba1de7a1597a054ef735d60e288c1b0d9d9fa8
Ricardo Contreras <ricardocontrerasrobles@gmail.com>
2015-12-04 18:18:15 -0500

    Returning padded json

--- server.py
+++ server.py
@@ -76,16 +76,16 @@ def hello():
     
 @app.route('/sessions/new', methods=['GET'])
 def new():
-    return jsonpify(create_session()), 201
+    return jsonpify(create_session())
 
 
 @app.route('/sessions/<session_id>/next', methods=['GET'])
 def next(session_id):
-    return jsonpify(on_next(session_id)), 200
+    return jsonpify(on_next(session_id))
 
 
 @app.route('/sessions/<session_id>/select/<term>', methods=['GET'])
 def select(session_id, term):
-    return jsonpify(on_select_term(session_id, term)), 200
+    return jsonpify(on_select_term(session_id, term))
 
 
