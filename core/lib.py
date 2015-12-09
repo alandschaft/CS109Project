554339531824b8dcc88d9af8b5e22c76676593d0
Ricardo Contreras <ricardocontrerasrobles@gmail.com>
2015-12-08 23:03:08 -0800

    New get docs

--- core/lib.py
+++ core/lib.py
@@ -13,15 +13,19 @@ def init_data():
     terms = pd.read_pickle(os.path.join(path, '..', 'data', 'terms.pckl'))
     docs = pd.read_pickle(os.path.join(path, '..', 'data', 'trials.pckl'))
     data['candidate_terms_init'] = terms.term.values.tolist()
-    data['candidate_docs_init'] = [list(doc['terms']) for doc in docs[['terms']].to_dict('records')]
-    data['ui_docs_full'] = docs[['nct_id', 'title', 'url']].to_dict('records')
-    data['ui_docs_init'] = get_docs(data['ui_docs_full'])
+    data['candidate_docs_init'] = init_ui_docs_full(docs)
+    # Holds the initial terms / docs lists we present with new sessions
     data['ui_terms_init'] = get_terms(data['candidate_terms_init'], data['candidate_docs_init'])
     return data
 
+def init_ui_docs_full(docs_df):
+    _docs = docs_df[['nct_id', 'title', 'url', 'terms']].to_dict('records')
+    for doc in _docs:
+        doc['terms'] = doc['terms'].tolist()
+    return _docs
 
 def get_freqs(terms, docs):
-    _docs = ('|'.join(doc) for doc in docs)
+    _docs = ('|'.join(doc['terms']) for doc in docs)
     cv = CountVectorizer(vocabulary=terms, analyzer=lambda doc: doc.split('|'))
     m = cv.transform(_docs)
     terms_freq = sorted(
@@ -39,7 +43,7 @@ def filter_terms(terms, excl_terms):
 
 def filter_docs(docs, curr_terms, m):
     _docs = [
-        doc for doc in docs if len(set(doc).intersection(set(curr_terms))) < m
+        doc for doc in docs if len(set(doc['terms']).intersection(set(curr_terms))) < m
     ]
     return _docs
 
@@ -61,7 +65,29 @@ def get_terms(terms, docs, n=40, k=10, m=5):
 
     return curr_terms_freq[:n]
 
+def get_tfidf_matrix_and_vectorizer(docs):
+        tfidf_vectorizer = TfidfVectorizer()
+        tfidf_matrix = tfidf_vectorizer.fit_transform(docs)
+        return tfidf_matrix, tfidf_vectorizer
 
-def get_docs(docs, n=40):
-    shuffle(docs)
-    return docs[:n]
+def get_docs(docs, selected_terms, ui_terms, n_docs=20):
+    '''
+        Returns a list of doc indices (indices in docs) that is sorted:
+        based on relevance to selected_terms and ui_terms
+        
+        Parameters:
+        docs - The target list of documents. Each document is a string. Words are separated by spaces.
+        selected_terms, ui_terms - The lists of terms. (each term is a string in the list)
+        n_docs - the number of document indices to output
+    '''
+    if not docs:
+        return []
+    total_terms = selected_terms + ui_terms;
+    # Create tf-idf vectorizer and matrix from the documents table
+    tfidf_matrix, tfidf_vectorizer = get_tfidf_matrix_and_vectorizer(docs)
+    # Vectorize the list of terms
+    tfidf_total_terms = tfidf_vectorizer.transform([' '.join(total_terms)])
+    # Calculate cosine similarities between the list of terms and each document
+    score = cosine_similarity(tfidf_total_terms, tfidf_matrix)
+    # Sort based on cosine similarities and return the indices of documents with higher scores
+    return score.argsort().flatten()[-n_docs:][::-1]
